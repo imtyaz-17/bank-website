3ad140149fe01cb18975198dee58511bf848a1ae
imtyaz-17 <imtyazit17017@gmail.com>
2023-09-24 11:50:11 +0600

    login js - added

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('btn-login').addEventListener('click', function () {
+    // get user email
+    const emailFild = document.getElementById('user-email');
+    const email = emailFild.value;
+    // get user password
+    const passFild = document.getElementById('user-pass');
+    const pass = passFild.value;
+    // check email and password
+    if (email == 'ab@gb.com' && pass == '121') {
+        window.location.href = 'banking.html';
+    } else {
+        alert('Wrong!!')
+    }
+})
