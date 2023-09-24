a9522419083db77f037bf0d28d95381046d41d7d
imtyaz-17 <imtyazit17017@gmail.com>
2023-09-24 15:48:31 +0600

    error handled

--- js/withdraw.js
+++ js/withdraw.js
@@ -7,22 +7,23 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
         alert('Please provide a valid number');
         return;
     }
-
-    const withdrawTotalElement = document.getElementById('withdraw-total');
-    const previousWithdrawTotalString = withdrawTotalElement.innerText;
-    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
-
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
-
-
     if (newWithdrawAmount > previousBalanceTotal) {
         alert('Insufficient Balance');
         return;
     }
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+
+
+
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
 
